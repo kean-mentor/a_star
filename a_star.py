4891121803cf068f4beded1e3fa109833e851e90
kesztea@gmail.com <kesztea@gmail.com>
2020-08-12 22:57:05 +0200

    Pathfinding + refactoring grid and screen coords

--- a_star.py
+++ a_star.py
@@ -1,3 +1,4 @@
+import queue
 import sys
 
 import pygame
@@ -15,14 +16,14 @@ surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def prepare_cell_data():
-    cols = WIDTH // SIZE
     rows = HEIGHT // SIZE
+    cols = WIDTH // SIZE
  
     grid = []
     for r in range(rows):
         grid.append([])
         for c in range(cols):
-            spot = Spot(c, r, rows)
+            spot = Spot(r, c)
             grid[r].append(spot)
 
     return grid
@@ -34,44 +35,100 @@ def draw_cells(win, grid):
             spot.draw(win)
 
 def draw_gridlines(win):
-    cols = WIDTH // SIZE
     rows = HEIGHT // SIZE
+    cols = WIDTH // SIZE
 
     for r in range(rows):
         pygame.draw.line(win, GREY, (0, r * SIZE), (WIDTH, r * SIZE))
-
     for c in range(cols):
         pygame.draw.line(win, GREY, (c * SIZE, 0), (c * SIZE, HEIGHT))
+
+def draw(win, grid):
+    win.fill(WHITE)
+    draw_cells(win, grid)
+    draw_gridlines(win)
+
+    pygame.display.update()
 
 
 def get_clicked_cell(m_pos):
     row = m_pos[1] // SIZE
     col = m_pos[0] // SIZE
-
     return row, col
 
 
 def reset_grid():
-    # FYI: Think about global variables :(
-    points[CellType.START] = None
-    points[CellType.END] = None
+    print("Not implemented yet...")
 
-    for row in grid:
-        for cell in row:
-            cell.celltype = CellType.NULL
+
+def reconstruct_path(came_from, current, draw_func):
+    while current in came_from:
+        current = came_from[current]
+        current.celltype = CellType.PATH
+        draw_func()
+
+def calculate_shortest_path(draw_func, grid, start, end):
+    """https://en.wikipedia.org/wiki/A*_search_algorithm"""
+
+    count = 0
+    open_set = queue.PriorityQueue()
+    open_set.put((0, count, start))
+    open_set_hash = {start}
+    came_from = {}
+
+    # g_score: Distance from start to current node
+    g_score = {spot: float("inf") for row in grid for spot in row}  # TODO: Using objects as dict keys, is it OK?
+    g_score[start] = 0
+    # f_score: Sum of g_score and ESTIMATED distance from current to end node
+    f_score = {spot: float("inf") for row in grid for spot in row}  # TODO: Using objects as dict keys, is it OK?
+    f_score[start] = start.distance(end)
+
+    while not open_set.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            reconstruct_path(came_from, current, draw_func)
+
+            start.celltype = CellType.START
+            end.celltype = CellType.END
+            return True
+
+        for neighbor in current.neighbors:
+            new_g_score = g_score[current] + 1  # All edges are weighted equally
+            # If this path to neighbor is better than any previous one. Record it!
+            if new_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = new_g_score
+                f_score[neighbor] = g_score[neighbor] + neighbor.distance(end)
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
+                    neighbor.celltype = CellType.OPEN
+
+        draw_func()
+        
+        if current != start:
+            current.celltype = CellType.CLOSED
+
+    return False
 
 
 grid = prepare_cell_data()
-points = {CellType.START: None, CellType.END: None}  # Important cell locations
+start = None  # Start point
+end = None  # Destination point
 is_started = False  # Pathfinding algorithm started or not
-is_drawing = False  # Start and end points placed and left mouse button is pressed
+is_drawing = False  # Start and destination points are placed and left mouse button is pressed
 is_running = True
 
 
 while is_running:
-    surface.fill(WHITE)
-    draw_cells(surface, grid)
-    draw_gridlines(surface)
+    draw(surface, grid)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -84,20 +141,21 @@ while is_running:
 
             if pygame.mouse.get_pressed()[0]:  # Left mouse button
                 if grid[row][col].celltype not in (CellType.START, CellType.END):
-                    if not points[CellType.START]:
-                        celltype = CellType.START
-                    elif not points[CellType.END]:
-                        celltype = CellType.END
+                    if not start:
+                        grid[row][col].celltype = CellType.START
+                        start = grid[row][col]
+                    elif not end:
+                        grid[row][col].celltype = CellType.END
+                        end = grid[row][col]
                     else:
-                        celltype = CellType.BARRIER
+                        grid[row][col].celltype = CellType.BARRIER
                         is_drawing = True
-
-                    points[celltype] = row, col
-                    grid[row][col].celltype = celltype  # FIXME: Barrier is also added but it is unnecessary
             elif pygame.mouse.get_pressed()[2]:  # Right mouse button
-                celltype = grid[row][col].celltype
+                if grid[row][col] == start:
+                    start = None
+                elif grid[row][col] == end:
+                    end = None
 
-                points[celltype] = None
                 grid[row][col].celltype = CellType.NULL
         if event.type == pygame.MOUSEMOTION:
             if is_drawing:
@@ -108,13 +166,20 @@ while is_running:
         if event.type == pygame.MOUSEBUTTONUP:
             is_drawing = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                pass
+            if event.key == pygame.K_SPACE and start and end:
+                is_started = True
+
+                for row in grid:
+                    for cell in row:
+                        cell.find_neighbors(grid)
+
+                calculate_shortest_path(lambda: draw(surface, grid), grid, start, end)
             if event.key == pygame.K_r:
                 reset_grid()
 
     clock.tick(120)
-    pygame.display.update()
 
 pygame.quit()
 sys.exit()
+
+# TODO: 'win/surface' and 'grid' are global variables. Some functions get them as params others not. Choose one method.
--- spot.py
+++ spot.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 from colors import cell_colors
@@ -5,15 +7,17 @@ from constants import CellType, SIZE
 
 
 class Spot:
-    def __init__(self, col, row, total_rows):
+    def __init__(self, row, col):
         self._row = row
         self._col = col
         self._x = col * SIZE
         self._y = row * SIZE
-        self._total_rows = total_rows
         self._celltype = CellType.NULL
-        self._color = cell_colors[self._celltype]
         self._neighbors = []
+
+    # TODO: Why!!!??? Is it because we use spot objects as dict keys?
+    def __lt__(self, other):
+        return False
 
     @property
     def celltype(self):
@@ -22,11 +26,42 @@ class Spot:
     @celltype.setter
     def celltype(self, celltype):
         self._celltype = celltype
-        self._color = cell_colors[self._celltype]
+
+    @property
+    def neighbors(self):
+        return self._neighbors
+
+    def find_neighbors(self, grid):
+        max_row = len(grid) - 1
+        max_col = len(grid[0]) - 1
+        
+        # UP
+        if self._row > 0 and grid[self._row - 1][self._col]._celltype != CellType.BARRIER:
+            self._neighbors.append(grid[self._row - 1][self._col])
+
+        # RIGHT
+        if self._col < max_col and grid[self._row][self._col + 1]._celltype != CellType.BARRIER:
+            self._neighbors.append(grid[self._row][self._col + 1])
+        
+        # DOWN
+        if self._row < max_row and grid[self._row + 1][self._col]._celltype != CellType.BARRIER:
+            self._neighbors.append(grid[self._row + 1][self._col])
+
+        # LEFT:
+        if self._col > 0 and grid[self._row][self._col - 1]._celltype != CellType.BARRIER:
+            self._neighbors.append(grid[self._row][self._col - 1])
+
+    def distance(self, other):
+        """Calculating distance from 'other' using Manhattan distance."""
+        return abs(other._col - self._col) + abs(other._row - self._row)
+
+    def distance_euc(self, other):
+        """Calculating distance from 'other' using Euclidean distance."""
+        return math.sqrt(pow(other._col - self._col, 2) + pow(other._row - self._row, 2))
     
     def draw(self, surface):
         pygame.draw.rect(
             surface,
-            self._color,
+            cell_colors[self._celltype],
             (self._x, self._y, SIZE, SIZE)
         )
